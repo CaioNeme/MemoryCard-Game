9b4dd604a48a97c328eaef28ab758d978701981c
Caio Neme <caiocesarneme@gmail.com>
2023-04-05 20:43:21 -0300

    quantidade-de-cartas-add

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 let QtdCartas = Number(prompt('Digite a quantidade de cartas: (4-14)'));
 let par = QtdCartas % 2;
-let listCard = [];
+let i = 0;
+let listCard;
+const carta = 
+    `<li class="carta">
+        <div class="frente lado">
+            <img src="imagens/back.png">
+        </div>
+        <div class="verso lado">
+            Verso   
+        </div>    
+    </li>`
 //! ver se a QtdCartas é maior que 14 se for reseta o codigo
 //! ver se a QtdCartas é par se nao for reseta o codigo
 
 while(par !== 0 || QtdCartas > 14 || QtdCartas == 0 || QtdCartas < 4){
     //reseta o codigo
-    QtdCartas = Number(prompt('Digite a quantidade de cartas: (2-14)'));
+    QtdCartas = Number(prompt('Digite a quantidade de cartas: (4-14)'));
     par = QtdCartas % 2;
 }
 
 //! se for par e for menor ou igual a 14 add cartas igual a QtdCartas
-while(lisCard.length === QtdCartas){
+while(i < QtdCartas){
     //?add cartas + 1
-    listCard = document.querySelector(".baralho");
-    listCard.innerHTML = 
-    `<li class="carta">
-    <div class="frente lado">
-        <img src="imagens/back.png">
-    </div>
-    <div class="verso lado">
-        Verso
-        
-    </div>    
-    </li>`
-    
+    let a = document.querySelector(".baralho")
+    a.innerHTML += carta;
+    console.log(a);
+    //listCard.innerHTML = listCard.innerHTML + carta;
+    i++;
 }
